2d8c6c906f9858829412cdf07e31bafb88d774a3
Caua-Girard <caua.girard2006@gmail.com>
2025-03-19 10:42:14 -0300

    Inicializando repositorio

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UnaMed.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
